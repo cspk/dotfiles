0614fc88eae99586005c4a3fde552a271c43a01e
Pavel Serebryakov <cspk@list.ru>
2021-02-07 17:03:28 +0500

    repo: add doom.d

---
+++ doom.d/config.el
@@ -0,0 +1,72 @@
+;;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
+
+;; Place your private configuration here! Remember, you do not need to run 'doom
+;; sync' after modifying this file!
+
+
+;; Some functionality uses this to identify you, e.g. GPG configuration, email
+;; clients, file templates and snippets.
+(setq user-full-name "John Doe"
+      user-mail-address "john@doe.com")
+
+;; Doom exposes five (optional) variables for controlling fonts in Doom. Here
+;; are the three important ones:
+;;
+;; + `doom-font'
+;; + `doom-variable-pitch-font'
+;; + `doom-big-font' -- used for `doom-big-font-mode'; use this for
+;;   presentations or streaming.
+;;
+;; They all accept either a font-spec, font string ("Input Mono-12"), or xlfd
+;; font string. You generally only need these two:
+;; (setq doom-font (font-spec :family "monospace" :size 12 :weight 'semi-light)
+;;       doom-variable-pitch-font (font-spec :family "sans" :size 13))
+
+;; There are two ways to load a theme. Both assume the theme is installed and
+;; available. You can either set `doom-theme' or manually load a theme with the
+;; `load-theme' function. This is the default:
+(setq doom-theme 'doom-cspk)
+
+;; If you use `org' and don't want your org files in the default location below,
+;; change `org-directory'. It must be set before org loads!
+(setq org-directory "~/org/")
+
+;; This determines the style of line numbers in effect. If set to `nil', line
+;; numbers are disabled. For relative line numbers, set this to `relative'.
+(setq display-line-numbers-type t)
+(setq doom-font (font-spec :family "Terminus" :size 16 :weight 'normal)
+      doom-variable-pitch-font (font-spec :family "Liberation Sans") ; inherits `doom-font''s :size
+      doom-unicode-font (font-spec :family "Liberation Sans" :size 12)
+      doom-big-font (font-spec :family "Liberation Sans" :size 19))
+
+(defun save-all ()
+  (interactive)
+  (save-some-buffers t))
+
+(add-function :after after-focus-change-function #'save-all)
+
+(setq-default cursor-type 'bar)
+(blink-cursor-mode t)
+(setq-default blink-cursor-blinks 0)
+(setq-default blink-cursor-delay 1)
+(setq-default blink-cursor-interval 1)
+
+(global-set-key (kbd "M-w") 'easy-kill)
+(global-set-key (kbd "C-x k") 'kill-current-buffer)
+
+;; Here are some additional functions/macros that could help you configure Doom:
+;;
+;; - `load!' for loading external *.el files relative to this one
+;; - `use-package!' for configuring packages
+;; - `after!' for running code after a package has loaded
+;; - `add-load-path!' for adding directories to the `load-path', relative to
+;;   this file. Emacs searches the `load-path' when you load packages with
+;;   `require' or `use-package'.
+;; - `map!' for binding new keys
+;;
+;; To get information about any of these functions/macros, move the cursor over
+;; the highlighted symbol at press 'K' (non-evil users must press 'C-c c k').
+;; This will open documentation for it, including demos of how they are used.
+;;
+;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
+;; they are implemented.
---
+++ doom.d/custom.el
@@ -0,0 +1,48 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ansi-color-names-vector
+   ["#282828" "#fb4934" "#b8bb26" "#fabd2f" "#83a598" "#cc241d" "#8ec07c" "#ebdbb2"])
+ '(blink-cursor-mode nil)
+ '(custom-safe-themes
+   '("e1b0c3f2ac2a5b51398d2ca6d12ecdc90bf2b2309ac2fefe1991554f5161a610" "7879a489f34f2565a4039076ea84a01a7eec27d21fae710a40a43bad1170a106" "0e31ff711ce9521f0afbd04f13a069f6243df7a1aa74a90c57c310f59d85bb77" "1bce653e69a005128853c19630eabb79a5e46ac7dc6b9192f2e1e4af4b1c6d00" "e13443da4ed37cffe5e23eea50b6e9eb399007acc884d378261dc25143bbed3e" "276790ba6767b04b53153c5dfbf27037b827cdd3685c66742c3d9e3ef0f6a258" "36dccd351ec47d4cdb663fccd9e5a3c5031787ec7b829398fd577b4b5c8f48bc" "e0a4eb9488dacb5a981fc499cd70a028dd0a954e8a91c61e87b959fd2daba027" "c4bdbbd52c8e07112d1bfd00fee22bf0f25e727e95623ecb20c4fa098b74c1bd" "c086fe46209696a2d01752c0216ed72fd6faeabaaaa40db9fc1518abebaf700d" "9efb2d10bfb38fe7cd4586afb3e644d082cbcdb7435f3d1e8dd9413cbe5e61fc" "4a8d4375d90a7051115db94ed40e9abb2c0766e80e228ecad60e06b3b397acab" "e6ff132edb1bfa0645e2ba032c44ce94a3bd3c15e3929cdf6c049802cf059a2a" "990e24b406787568c592db2b853aa65ecc2dcd08146c0d22293259d400174e37" "d6603a129c32b716b3d3541fc0b6bfe83d0e07f1954ee64517aa62c9405a3441" "76bfa9318742342233d8b0b42e824130b3a50dcc732866ff8e47366aed69de11" "4bca89c1004e24981c840d3a32755bf859a6910c65b829d9441814000cf6c3d0" default))
+ '(doom-big-font-mode nil)
+ '(fci-rule-color "#7c6f64")
+ '(jdee-db-active-breakpoint-face-colors (cons "#0d1011" "#fabd2f"))
+ '(jdee-db-requested-breakpoint-face-colors (cons "#0d1011" "#b8bb26"))
+ '(jdee-db-spec-breakpoint-face-colors (cons "#0d1011" "#928374"))
+ '(objed-cursor-color "#fb4934")
+ '(package-selected-packages '(easy-kill))
+ '(pdf-view-midnight-colors (cons "#ebdbb2" "#282828"))
+ '(rustic-ansi-faces
+   ["#282828" "#fb4934" "#b8bb26" "#fabd2f" "#83a598" "#cc241d" "#8ec07c" "#ebdbb2"])
+ '(vc-annotate-background "#282828")
+ '(vc-annotate-color-map
+   (list
+    (cons 20 "#b8bb26")
+    (cons 40 "#cebb29")
+    (cons 60 "#e3bc2c")
+    (cons 80 "#fabd2f")
+    (cons 100 "#fba827")
+    (cons 120 "#fc9420")
+    (cons 140 "#fe8019")
+    (cons 160 "#ed611a")
+    (cons 180 "#dc421b")
+    (cons 200 "#cc241d")
+    (cons 220 "#db3024")
+    (cons 240 "#eb3c2c")
+    (cons 260 "#fb4934")
+    (cons 280 "#e05744")
+    (cons 300 "#c66554")
+    (cons 320 "#ac7464")
+    (cons 340 "#7c6f64")
+    (cons 360 "#7c6f64")))
+ '(vc-annotate-very-old-color nil))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
