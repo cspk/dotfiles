9461cb19583d64a4c48c98a0b9e34663dd500ac0
Pavel Serebryakov <cspk@list.ru>
2022-11-14 00:41:31 +0500

    doom: change custom-theme-directory

--- doom.d/custom.el
+++ doom.d/custom.el
@@ -3,6 +3,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("631c52620e2953e744f2b56d102eae503017047fb43d65ce028e88ef5846ea3b" "7a424478cb77a96af2c0f50cfb4e2a88647b3ccca225f8c650ed45b7f50d9525" default))
  '(package-selected-packages '(total-lines)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
